d959071886e2fb1998501b3fd944e06d8e38e55b
Axel Hodler <axel@hodler.co>
2017-12-10 19:16:34 +0100

    init from start.spring.io

---
+++ src/main/java/co/hodler/boundaries/BoundariesApplication.java
@@ -0,0 +1,12 @@
+package co.hodler.boundaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoundariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoundariesApplication.class, args);
+	}
+}
